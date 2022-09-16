ca548ecda074307897812ee5b1a2b1f6e5ba82a8
Maria Stylianou <marsty5@gmail.com>
2022-09-16 15:40:48 +0300

    Move html text to route.js for exercises 1 and 2, for consistency

--- app/pods/exercises/html-css/1/route.js
+++ app/pods/exercises/html-css/1/route.js
@@ -1,9 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    steps: [
+        {
+            title: "Η δομή της σελίδας ",
+            description: "Δημιουργήστε τη δομή της HTML σελίδας.",
+            information: ""
+        },
+        {
+            title: "Το element 'title'",
+            description: "Βάλτε τίτλο πάνω στο tab της σελίδας σας.",
+            information: " Πως: προσθέστε το element 'title' στο head κομμάτι της σελίδας."
+        },
+        {
+            title: "Επικεφαλίδα",
+            description: "Προσθέστε ένα τίτλο στο περιεχόμενο της σελίδας. Χρησιμοποιήστε το element 'h2' στο body κομμάτι της σελίδας."
+
+        }
+    ],
     model(){
         return {
-            tab: 'result'
+            tab: 'result',
+            steps: this.get('steps')
         };
     },
     actions: {
--- app/pods/exercises/html-css/2/route.js
+++ app/pods/exercises/html-css/2/route.js
@@ -1,9 +1,41 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    steps: [
+        {
+            title: "Η δομή της σελίδας ",
+            description: "Δημιουργήστε τη δομή της HTML σελίδας.",
+            information: ""
+        },
+        {
+            title: "Το element 'title'",
+            description: "Βάλτε τίτλο πάνω στο tab της σελίδας σας.",
+            information: " Πως: προσθέστε το element 'title' στο head κομμάτι της σελίδας."
+        },
+        {
+            title: "Επικεφαλίδα",
+            description: "Προσθέστε ένα τίτλο στο περιεχόμενο της σελίδας. Χρησιμοποιήστε το element 'h2' στο body κομμάτι της σελίδας.",
+            information: ""
+        },
+        {
+            title: "Παράγραφος",
+            description: "Προσθέστε το element 'p' στο body κομμάτι της σελίδας.",
+            information: ""
+        },
+        {
+            title: "Εικόνα",
+            description: "Προσθέστε το element 'img', συνδέοντας το με ένα λινκ σε μια εικόνα μέσα στο attribute 'src'.",
+            information: ""
+        },
+        {
+            title: "Λίστα",
+            description: "Προσθέστε μια αριθμημένη λίστα όπου σε σειρά προτεραιότητας, καταγράφετε τις αγαπημένες σας δραστηριότητες. Χρησιμοποιήστε τα elements 'ol' και 'li'."
+        }
+    ],
     model(){
         return {
-            tab: 'result'
+            tab: 'result',
+            steps: this.get('steps')
         };
     },
     actions: {
