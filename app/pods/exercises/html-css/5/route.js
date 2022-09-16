ebd7018767578405a8d60f044eee643227530551
Maria Stylianou <marsty5@gmail.com>
2022-09-16 16:29:08 +0300

    Change CSS exercises, 5 and 6

--- app/pods/exercises/html-css/5/route.js
+++ app/pods/exercises/html-css/5/route.js
@@ -3,32 +3,42 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     steps: [
         {
-            title: 'Page structure & Title',
-            description: 'Create the structure of an HTML page and give it a title'
+            title: "Δημιουργία html αρχείου",
+            description: "Μέσα στον ίδιο φάκελο 'website-1', δημιουργήστε ένα νέο αρχείο και αποθηκεύστε το ως 'index2.html'.",
+            information: ""
         },
         {
-            title: 'Welcome!',
-            description: 'Add an h1 element, and write "Welcome!"'
+            title: "Η δομή της σελίδας",
+            description: "Δημιουργήστε τη δομή της HTML σελίδας.",
+            information: "❗Θυμηθείτε τα elements: html, head, body, title."
         },
         {
-            title: 'Tagline',
-            description: 'Add an h3 element to act as the tagline of your page!'
+            title: "Τίτλος 'Τι κάνω όταν...'",
+            description: "Δημιουργήστε ένα τίτλο h3."
         },
         {
-            title: 'My favorite songs are...',
-            description: 'Create a list with three of your favorite songs'
+            title: "'... χειμωνιάζει'",
+            description: "Δημιουργήστε μια λίστα (bullet-points) με 2-3 δραστηριότητες που κάνετε το χειμώνα."
         },
         {
-            title: 'Dive into CSS!',
-            description: 'Create a css file by creating a file named “example5.css” in the same folder as your .html file.'
+            title: "'... καλοκαιριάζει'",
+            description: "Δημιουργήστε μια λίστα (bullet-points) με 2-3 δραστηριότητες που κάνετε το καλοκαίρι."
         },
         {
-            title: 'HTML + CSS = <3!',
-            description: 'Import your css file in the head of your HTML page, by utilising the link tag.'
+            title: "Δημιουργία CSS αρχείου!",
+            description: "Δημιουργήστε το φάκελο 'css' αν δεν τον έχετε ήδη. Μέσα στο 'css' φάκελο, δημιουργήστε ένα αρχείο με το όνομα style.css."
         },
         {
-            title: 'CSS tag selectors!',
-            description: 'Create an “li” selector in your css file and set the color attribute to blue. Similarly, create a “h1” and “h2” tag selector and set the color to red. (Hint: Use comma ,)'
+            title: "Συνδέστε τα html και css αρχεία σας!",
+            description: "Στο html αρχέιο, μέσα στο head element, προσθέστε το element <link href='css/style.css' rel='stylesheet'>"
+        },
+        {
+            title: "CSS selector πάνω στο h3 element",
+            description: "Αλλάξτε το χρώμα του τίτλου σε κόκκινο"
+        },
+        {
+            title: "CSS selector πάνω στα li elements",
+            description: "Αλλάξτε το χρώμα όλων των αντικειμένων της λίστας σε πράσινο."
         }
     ],
     model(){
