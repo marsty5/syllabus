5a53c2ad3467ab3cd7316784e59e8b89bd6278b4
Maria Stylianou <marsty5@gmail.com>
2022-09-17 22:04:28 +0300

    make bootstrap exercises 2 and 3 simpler in html tab

--- app/pods/exercises/bootstrap/5/route.js
+++ app/pods/exercises/bootstrap/5/route.js
@@ -9,20 +9,8 @@ Click the button and see what happens!
 export default Ember.Route.extend({
     steps: [
         {
-            title: 'Navbar',
-            description: 'Add the top navigation bar with a title and a link. Check Bootstrap\'s Navbar documentation found in the links section.'
-        },
-        {
-            title: 'Section grey area',
-            description: 'To create the grey bottom section, you can use  "card-footer" class inside the card you want.'
-        },
-        {
-            title: 'Add fonts!',
-            description: 'Download the fonts zip provided in the links section, and place the fonts directory in the same folder as your .html file.'
-        },
-        {
-            title: 'Icons',
-            description: 'Add a few icons to decorate your description and your information. You can use icons by adding <i> tags with font-awesome classes. e.g: "<i class="fa fa-twitter"></i>".  More information found in the font awesome link in the links section.'
+            title: 'Δημιουργία style.css',
+            description: 'Δημιουργήστε το style.css σας και αρχίστε να κάνετε αλλαγές στα χρώματα!'
         }
     ],
     model(){
